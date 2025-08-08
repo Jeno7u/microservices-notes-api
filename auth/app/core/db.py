8b8adca22517e1a6af6c5a82247de0d46766bec1
Mironov Boris <lboris201@yandex.ru>
2025-08-08 12:59:30 +0300

    Added ECHO env variable for better adjustments

--- auth/app/core/db.py
+++ auth/app/core/db.py
@@ -4,9 +4,9 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 
 DATABASE_URL = os.getenv("ASYNC_DATABASE_URL")
+ECHO = bool(os.getenv("ECHO"))
 
-engine = create_async_engine(DATABASE_URL, echo=True)
-
+engine = create_async_engine(DATABASE_URL, echo=ECHO)
 async_session = async_sessionmaker(engine, expire_on_commit=False)
 
 
