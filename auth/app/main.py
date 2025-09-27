95f9c446a4270d636494d6619a7a1c3a6d9332bd
Mironov Boris <lboris201@yandex.ru>
2025-09-27 15:04:17 +0300

    Added Prometheus, Loki, Promtail and Grafana

--- auth/app/main.py
+++ auth/app/main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
 from auth.app.api.v1.auth import router as auth_router
-from prometheus_fastapi_instrumentator import Instrumentator
+
 
 app = FastAPI()
 app.include_router(auth_router, prefix="/auth", tags=["Auth"])
 
-Instrumentator().instrument(app).expose(app)
--- gateway/app/main.py
+++ gateway/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
 from app.api.v1.auth import router as auth_router
 from app.api.v1.notes import router as notes_router
@@ -7,3 +8,6 @@ from app.api.v1.notes import router as notes_router
 app = FastAPI()
 app.include_router(auth_router, prefix="/auth", tags=["Auth"])
 app.include_router(notes_router, prefix="/notes", tags=["Notes"])
+
+
+Instrumentator().instrument(app).expose(app)
